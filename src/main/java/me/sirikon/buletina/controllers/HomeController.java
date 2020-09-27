1cb0438f19ea57c5a90377c5755886f315c625c4
Carlos Fdez. Llamas <hello@sirikon.me>
2020-09-28 01:24:19 +0200

    Label as singleton

--- src/main/java/me/sirikon/buletina/controllers/HomeController.java
+++ src/main/java/me/sirikon/buletina/controllers/HomeController.java
@@ -4,7 +4,9 @@ import io.javalin.http.Context;
 import me.sirikon.buletina.services.TemplateService;
 
 import javax.inject.Inject;
+import javax.inject.Singleton;
 
+@Singleton
 public class HomeController {
 
   private final TemplateService templateService;
