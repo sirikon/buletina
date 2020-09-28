8786715edaf29e57423726d9e1591292bb6a4c80
Carlos Fdez. Llamas <hello@sirikon.me>
2020-09-28 19:31:56 +0200

    Discard template resource listing as it doesn't work on fatjar because some reason

--- src/main/java/me/sirikon/buletina/services/TemplateService.java
+++ src/main/java/me/sirikon/buletina/services/TemplateService.java
@@ -5,33 +5,44 @@ import com.github.mustachejava.Mustache;
 
 import javax.inject.Inject;
 import javax.inject.Singleton;
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.io.InputStreamReader;
 import java.io.StringWriter;
-import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
-import java.util.stream.Collectors;
 
 @Singleton
 public class TemplateService {
+
+  private static final List<String> AVAILABLE_TEMPLATES = List.of(
+      "index.html"
+  );
 
   private final Map<String, Mustache> templates;
 
   @Inject
   public TemplateService() {
     templates = new HashMap<>();
-    final var availableTemplates = getAvailableTemplates();
     final var mustacheFactory = new DefaultMustacheFactory();
-    availableTemplates.forEach((t) -> {
-      final var tplStream = getClass().getClassLoader().getResourceAsStream("templates/" + t);
-      if (tplStream == null) {
-        throw new RuntimeException("Template '" + t + "' not found");
+    AVAILABLE_TEMPLATES.forEach((t) -> {
+      final var possibleFileInDisk = new File("templates/" + t);
+      if (possibleFileInDisk.exists()) {
+        try {
+          final var tplStream = new FileInputStream(possibleFileInDisk);
+          templates.put(t, mustacheFactory.compile(new InputStreamReader(tplStream), t));
+        } catch (FileNotFoundException e) {
+          throw new RuntimeException(t);
+        }
+      } else {
+        final var tplStream = getClass().getClassLoader().getResourceAsStream("templates/" + t);
+        if (tplStream == null) {
+          throw new RuntimeException("Template '" + t + "' not found");
+        }
+        templates.put(t, mustacheFactory.compile(new InputStreamReader(tplStream), t));
       }
-      templates.put(t, mustacheFactory.compile(new InputStreamReader(tplStream), t));
     });
   }
 
@@ -41,41 +52,41 @@ public class TemplateService {
     return writer.toString();
   }
 
-  private List<String> getAvailableTemplates() {
-    try {
-      return getResourceFiles("templates");
-    } catch (final Throwable t) {
-      throw new RuntimeException(t);
-    }
-  }
-
-  private List<String> getResourceFiles(final String path) throws IOException {
-    List<String> filenames = new ArrayList<>();
-
-    InputStream in = getResourceAsStream(path);
-    BufferedReader br = new BufferedReader(new InputStreamReader(in));
-
-    String resource;
-    while ((resource = br.readLine()) != null) {
-      if (!resource.contains(".")) {
-        final var parent = resource;
-        filenames.addAll(getResourceFiles(path + "/" + resource).stream()
-            .map((r) -> parent + "/" + r).collect(Collectors.toList()));
-        continue;
-      }
-      filenames.add(resource);
-    }
-
-    return filenames;
-  }
-
-  private InputStream getResourceAsStream(final String resource) {
-    final InputStream in = getContextClassLoader().getResourceAsStream(resource);
-    return in == null ? getClass().getResourceAsStream(resource) : in;
-  }
-
-  private ClassLoader getContextClassLoader() {
-    return Thread.currentThread().getContextClassLoader();
-  }
+//  private List<String> getAvailableTemplates() {
+//    try {
+//      return getResourceFiles("templates");
+//    } catch (final Throwable t) {
+//      throw new RuntimeException(t);
+//    }
+//  }
+//
+//  private List<String> getResourceFiles(final String path) throws IOException {
+//    List<String> filenames = new ArrayList<>();
+//
+//    InputStream in = getResourceAsStream(path);
+//    BufferedReader br = new BufferedReader(new InputStreamReader(in));
+//
+//    String resource;
+//    while ((resource = br.readLine()) != null) {
+//      if (!resource.contains(".")) {
+//        final var parent = resource;
+//        filenames.addAll(getResourceFiles(path + "/" + resource).stream()
+//            .map((r) -> parent + "/" + r).collect(Collectors.toList()));
+//        continue;
+//      }
+//      filenames.add(resource);
+//    }
+//
+//    return filenames;
+//  }
+//
+//  private InputStream getResourceAsStream(final String resource) {
+//    final InputStream in = getContextClassLoader().getResourceAsStream(resource);
+//    return in == null ? getClass().getResourceAsStream(resource) : in;
+//  }
+//
+//  private ClassLoader getContextClassLoader() {
+//    return Thread.currentThread().getContextClassLoader();
+//  }
 
 }
