f27d86e50a80b4dbde97fe2757f12d8863dedf15
Carlos Fdez. Llamas <hello@sirikon.me>
2020-09-30 19:38:20 +0200

    Load config from env vars

--- src/main/java/me/sirikon/buletina/configuration/Configuration.java
+++ src/main/java/me/sirikon/buletina/configuration/Configuration.java
@@ -1,5 +1,7 @@
 package me.sirikon.buletina.configuration;
 
+import com.google.common.base.Strings;
+
 import javax.inject.Singleton;
 
 @Singleton
@@ -8,11 +10,19 @@ public class Configuration {
   private final String jwtSecret;
 
   public Configuration() {
-    this.jwtSecret = "secret";
+    this.jwtSecret = requireEnvironmentVariable("BULETINA_JWT_SECRET");
   }
 
   public String getJwtSecret() {
     return jwtSecret;
+  }
+
+  private static String requireEnvironmentVariable(final String key) {
+    final var value = System.getenv(key);
+    if (Strings.isNullOrEmpty(value)) {
+      throw new RuntimeException("Environment variale '" + key + "' is required");
+    }
+    return value;
   }
 
 }
